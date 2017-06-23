f8b2e717724185d6e827a2053a09b6296e293f10
Jian Zhang <mastercoder225@gmail.com>
2017-06-23 08:03:58 +0800

    fix

--- src/main/java/com/example/concurrent/JavaExecutorService.java
+++ src/main/java/com/example/concurrent/JavaExecutorService.java
@@ -1,8 +1,13 @@
 package com.example.concurrent;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class JavaExecutorService {
 
     public static void main(String[] args) {
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        es.sub
 
     }
 }
