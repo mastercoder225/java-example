347fe53e76775bb3137b874e552dc17f25f0ea50
Jian Zhang <mastercoder225@gmail.com>
2017-06-26 13:22:46 +0800

    fix

---
+++ src/main/java/com/example/designpattern/builder/AppContext.java
@@ -0,0 +1,13 @@
+package com.example.designpattern.builder;
+
+public class AppContext {
+
+    public static void main(String[] args) {
+        Component component = Component.builder()
+                .addPartA("Part A")
+                .addPartB("Part B")
+                .addPartC("Part C")
+                .build();
+        System.out.println(component);
+    }
+}
