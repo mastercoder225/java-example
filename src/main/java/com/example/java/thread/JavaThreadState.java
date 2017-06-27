31c70f39fab431bf6bafb5cf1d5a822bdf89e10d
Jian Zhang <mastercoder225@gmail.com>
2017-06-28 01:43:42 +0800

    fix

---
+++ src/main/java/com/example/java/thread/JavaThreadState.java
@@ -0,0 +1,12 @@
+package com.example.java.thread;
+
+public class JavaThreadState {
+
+    public static void main(String[] args) {
+        // NEW|RUNNABLE|BLOCKED|WAITING|TIMED_WAITING|TERMINATED
+
+        // NEW
+        Thread t = new Thread();
+
+    }
+}
