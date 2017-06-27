d3f950fc3b50a6dc2d516ecc2978dbb37652b0d6
Jian Zhang <mastercoder225@gmail.com>
2017-06-27 13:22:50 +0800

    fix

--- src/main/java/com/example/java/designpattern/strategy/AppTest.java
+++ src/main/java/com/example/java/designpattern/strategy/AppTest.java
@@ -3,7 +3,5 @@ package com.example.java.designpattern.strategy;
 public class AppTest {
 
     public static void main(String[] args) {
-        AppContext appContext = new AppContext(new ConcreteStrategyB());
-        appContext.applyStrategy();
     }
 }
