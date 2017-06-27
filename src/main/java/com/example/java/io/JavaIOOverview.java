99155928cbab2fa547174d08aef15d1e8b1c1bac
Jian Zhang <mastercoder225@gmail.com>
2017-06-27 18:44:06 +0800

    fix

---
+++ src/main/java/com/example/java/io/JavaIOOverview.java
@@ -0,0 +1,18 @@
+package com.example.java.io;
+
+public class JavaIOOverview {
+
+    public static void main(String[] args) {
+        // source -> program -> destination
+
+        // common sources and destinations
+        // 1. files
+        // 2. pipes
+        // 3. network connections
+        // 4. in-memory buffers
+        // 5. System.in, System.out, System.err
+
+        // source -> InputStream|Reader -> program
+        // program -> OutputStream|Writer -> destination
+    }
+}
