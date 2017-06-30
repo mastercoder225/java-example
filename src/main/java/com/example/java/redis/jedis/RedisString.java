a215ec7700c327dc5deca093b866d10d50d803e3
Jian Zhang <mastercoder225@gmail.com>
2017-06-30 19:01:05 +0800

    fix

--- src/main/java/com/example/java/redis/jedis/RedisString.java
+++ src/main/java/com/example/java/redis/jedis/RedisString.java
@@ -11,24 +11,26 @@ public class RedisString {
         jedis.set("K", "V2"); // OK K:V2
         jedis.set("K", "V3", "NX"); // NULL
         jedis.set("K", "V4", "XX"); // OK K:V4
-        jedis.set("K", "V5", "XX", "EX", 100);    // OK K:V5 100s
-        jedis.set("K", "V6", "XX", "PX", 100000); // OK K:V6 100000ms
+        jedis.set("K", "V5", "XX", "EX", 10);    // OK K:V5 10s
+        jedis.set("K", "V6", "XX", "PX", 10000); // OK K:V6 10000ms
 
         jedis.get("K"); // V6
         jedis.getSet("K", "RedisString"); // V6 K:RedisString
         jedis.getrange("K", 0, -1);  // RedisString
-        jedis.getrange("K", 0, 100); // RedisString
-        jedis.getrange("K", 0, 4);   // Redis
-        jedis.getrange("K", 0, -7);  // Redis
-        jedis.getrange("K", 5, -1);  // String
-        jedis.getrange("K", -6, -1); // String
+        jedis.getrange("K", 0, -4);  // RedisStr
+        jedis.getrange("K", 2, 7);   // disStr
+        jedis.getrange("K", 2, -4);  // disStr
+        jedis.getrange("K", -9, -4); // disStr
+        jedis.getrange("K", -9, 7);  // disStr
 
-        jedis.set("K", "1");  // OK K:1
-        jedis.incr("K");      // 2  K:2
-        jedis.incrBy("K", 5); // 7  K:7
-        jedis.decr("K");      // 6  K:6
-        jedis.decrBy("K", 5); // 1  K:1
-        jedis.incrByFloat("K", 1);   // 2   K:2
-        jedis.incrByFloat("K", 1.5); // 3.5 K:3.5
+        jedis.incr("K_INT");      // 1 K_INT:1
+        jedis.incrBy("K_INT", 5); // 6 K_INT:6
+        jedis.decr("K_INT");      // 5 K_INT:5
+        jedis.decrBy("K_INT", 5); // 0 K_INT:0
+        jedis.incrByFloat("K_INT", 1.0); // 1.0 K_INT:1
+        jedis.incrByFloat("K_INT", 1.5); // 2.5 K_INT:2.5
+
+        jedis.del("K"); // 1
+        jedis.del("K_INT", "K_BIT"); // 2
     }
 }
