590e8442bb3561d38a21621dcb9147c49ee41498
Jian Zhang <mastercoder225@gmail.com>
2017-07-04 14:40:51 +0800

    fix

--- src/main/java/com/example/java/redis/jedis/RedisHash.java
+++ src/main/java/com/example/java/redis/jedis/RedisHash.java
@@ -1,8 +1,22 @@
 package com.example.java.redis.jedis;
 
+import redis.clients.jedis.Jedis;
+
 public class RedisHash {
 
     public static void main(String[] args) {
+        Jedis jedis = new Jedis();
 
+        jedis.hset("K", "F1", "V1"); // 1 K:{F1:V1}
+        jedis.hset("K", "F2", "V2"); // 1 K:{F1:V1,F2:V2}
+        jedis.hset("K", "F3", "V3"); // 1 K:{F1:V1,F2:V2,F3:V3}
+        jedis.hget("K", "F1"); // V1
+        jedis.hgetAll("K"); // {F1:V1,F2:V2,F3:V3}
+        jedis.hexists("K", "F1"); // true
+        jedis.hlen("K"); // 3
+        jedis.hkeys("K"); // {F1,F2,F3}
+        jedis.hvals("K"); // {V1,V2,V3}
+
+        jedis.del("K");
     }
 }
