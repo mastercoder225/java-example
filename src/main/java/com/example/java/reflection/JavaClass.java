e751bbd0d4a1225a9c808c941fa3b2cc61c2e826
zhangjian <zhangjian@kongzhong.com>
2017-07-22 16:12:42 +0800

    fix

--- src/main/java/com/example/java/reflection/JavaClass.java
+++ src/main/java/com/example/java/reflection/JavaClass.java
@@ -1,24 +1,74 @@
 package com.example.java.reflection;
 
+import lombok.Data;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class JavaClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Class<Integer> clazz = Integer.class;
 
-        clazz.toString(); // class java.lang.Integer
-        clazz.toGenericString(); // public final class java.lang.Integer
+        // name
+        String name = clazz.getName(); // java.lang.Integer
+        String simpleName = clazz.getSimpleName(); // Integer
+        String canonicalName = clazz.getCanonicalName(); // java.lang.Integer
+        String typeName = clazz.getTypeName(); // java.lang.Integer
 
-        clazz.getName();
-        clazz.getSimpleName();
-        clazz.getModifiers();
-        clazz.getPackage();
-        clazz.getSuperclass();
-        clazz.getInterfaces();
-        clazz.getConstructors();
-        clazz.getFields();
-        clazz.getMethods();
-        clazz.getAnnotations();
+        // public constructor
+        Constructor<Integer> constructor = clazz.getConstructor(String.class);
+        Constructor<?>[] constructors = clazz.getConstructors();
 
-        System.out.println(clazz.toGenericString());
+        // public|protected|default|private constructor
+        Constructor<Integer> declaredConstructor = clazz.getDeclaredConstructor(String.class);
+        Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+
+        // public method
+        Method method = clazz.getMethod("valueOf", String.class);
+        Method[] methods = clazz.getMethods();
+
+        // public|protected|default|private method
+        Method declaredMethod = clazz.getDeclaredMethod("valueOf", String.class);
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+
+        // public field
+        Field field = clazz.getField("MAX_VALUE");
+        Field[] fields = clazz.getFields();
+
+        // public|protected|default|private field
+        Field declaredField = clazz.getDeclaredField("MAX_VALUE");
+        Field[] declaredFields = clazz.getDeclaredFields();
+
+        // annotation
+        Annotation[] annotations = clazz.getAnnotations();
+        Annotation[] declaredAnnotations = clazz.getDeclaredAnnotations();
+
+        // interface
+        Class<?>[] interfaces = clazz.getInterfaces();
+
+        // modifiers
+        int modifiers = clazz.getModifiers();
+
+        // package
+        Package p = clazz.getPackage();
+
+        // classloader
+        ClassLoader classLoader = clazz.getClassLoader();
+
+        boolean isAnnotation = clazz.isAnnotation();
+        //boolean isAnnotationPresent = clazz.isAnnotationPresent(Data.class);
+        boolean isAnonymousClass = clazz.isAnonymousClass();
+        boolean isArray = clazz.isArray();
+        boolean isEnum = clazz.isEnum();
+        boolean isPrimitive = clazz.isPrimitive();
+        boolean isInterface = clazz.isInterface();
+        boolean isInstance = clazz.isInstance(new Integer("1"));
+        boolean isLocalClass = clazz.isLocalClass();
+        boolean isMemberClass = clazz.isMemberClass();
+        boolean isAssignableFrom = clazz.isAssignableFrom(Integer.class);
+        boolean isSynthetic = clazz.isSynthetic();
     }
 }
