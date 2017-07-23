d16f1378f951796092299320870462ac2f4cf8eb
zhangjian <zhangjian@kongzhong.com>
2017-07-23 15:54:20 +0800

    fix

--- src/main/java/com/example/java/netty/discardserver/DiscardServerHandler.java
+++ src/main/java/com/example/java/netty/discardserver/DiscardServerHandler.java
@@ -3,12 +3,18 @@ package com.example.java.netty.discardserver;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
 
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ((ByteBuf) msg).release();
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            System.out.print(in.toString(CharsetUtil.US_ASCII));
+        } finally {
+            in.release();
+        }
     }
 
     @Override
--- src/main/java/com/example/java/nio/JavaBuffer.java
+++ src/main/java/com/example/java/nio/JavaBuffer.java
@@ -1,20 +1,33 @@
 package com.example.java.nio;
 
 import java.nio.Buffer;
-import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 
 public class JavaBuffer {
 
     public static void main(String[] args) {
-        Buffer buffer = ByteBuffer.allocate(100);
+        Buffer buffer = CharBuffer.allocate(10);
 
-        int capacity = buffer.capacity();
-        int limit = buffer.limit();
+        // query operations
+        // 0 <= mark <= position <= limit <= capacity
         int position = buffer.position();
+        int limit = buffer.limit();
+        int capacity = buffer.capacity();
+        boolean hasRemaining = buffer.hasRemaining();
+        int remaining = buffer.remaining();
+        boolean hasArray = buffer.hasArray();
+        int arrayOffset = buffer.arrayOffset();
+        Object array = buffer.array();
+        boolean isDirect = buffer.isDirect();
+        boolean isReadOnly = buffer.isReadOnly();
 
-        buffer.mark();
-        buffer.reset();
-
-        buffer.clear();
+        // modification operations
+        Buffer buffer1 = buffer.clear();
+        Buffer buffer2 = buffer.flip();
+        Buffer buffer3 = buffer.rewind();
+        Buffer buffer4 = buffer.mark();
+        Buffer buffer5 = buffer.reset();
+        Buffer buffer6 = buffer.position(5);
+        Buffer buffer7 = buffer.limit(5);
     }
 }
