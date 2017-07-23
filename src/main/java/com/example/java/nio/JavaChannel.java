18f3734279466db1c61948b1d01f8504fc0e2034
zhangjian <zhangjian@kongzhong.com>
2017-07-23 18:32:12 +0800

    fix

--- src/main/java/com/example/java/nio/JavaChannel.java
+++ src/main/java/com/example/java/nio/JavaChannel.java
@@ -1,4 +1,18 @@
 package com.example.java.nio;
 
+import java.io.IOException;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.*;
+
 public class JavaChannel {
+
+    public static void main(String[] args) throws IOException {
+        Channel channel = FileChannel.open(Paths.get("test.txt"), READ, WRITE, TRUNCATE_EXISTING);
+
+        boolean isOpen = channel.isOpen();
+        channel.close();
+    }
 }
--- src/main/java/com/example/java/nio/channels/JavaFileChannel.java
+++ src/main/java/com/example/java/nio/channels/JavaFileChannel.java
@@ -1,4 +1,55 @@
 package com.example.java.nio.channels;
 
+import java.nio.ByteBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
+
 public class JavaFileChannel {
+
+    public static void main(String[] args) throws Exception {
+        FileChannel fileChannel = FileChannel.open(Paths.get("test.txt"));
+
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(100);
+
+        while (byteBuffer.hasRemaining()) {
+            System.out.println(fileChannel.write(byteBuffer));
+        }
+
+        fileChannel.force(true);
+
+
+
+
+
+        System.out.println(fileChannel.size());
+
+
+
+        boolean isOpen = fileChannel.isOpen();
+
+        System.out.println(isOpen);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        fileChannel.close();
+
+    }
 }
