11b7fb379e41106321e536f403f3556ff1723f63
zhangjian <zhangjian@kongzhong.com>
2017-07-23 19:10:21 +0800

    fix

--- src/main/java/com/example/java/nio/channels/JavaServerSocketChannel.java
+++ src/main/java/com/example/java/nio/channels/JavaServerSocketChannel.java
@@ -1,4 +1,20 @@
 package com.example.java.nio.channels;
 
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
 public class JavaServerSocketChannel {
+
+    public static void main(String[] args) throws Exception {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress("localhost", 8080));
+
+        while (true) {
+            SocketChannel socketChannel = serverSocketChannel.accept();
+
+            // do something with socketChannel
+        }
+
+    }
 }
--- src/main/java/com/example/java/nio/channels/JavaSocketChannel.java
+++ src/main/java/com/example/java/nio/channels/JavaSocketChannel.java
@@ -1,4 +1,24 @@
 package com.example.java.nio.channels;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+
 public class JavaSocketChannel {
+
+    public static void main(String[] args) throws Exception {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.configureBlocking(false);
+        socketChannel.connect(new InetSocketAddress("localhost", 8080));
+
+        socketChannel.finishConnect();
+
+
+
+
+
+
+
+        socketChannel.close();
+    }
 }
