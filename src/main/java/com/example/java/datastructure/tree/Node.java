5b8534fa007d68503026d99e5b0f67ea1da8ae2f
zhangjian <zhangjian@kongzhong.com>
2017-07-25 11:47:47 +0800

    fix

---
+++ src/main/java/com/example/java/datastructure/tree/Node.java
@@ -0,0 +1,10 @@
+package com.example.java.datastructure.tree;
+
+import lombok.Data;
+
+@Data
+public class Node {
+    private int data;
+    private Node left;
+    private Node right;
+}
---
+++ src/main/java/com/example/java/datastructure/tree/Tree.java
@@ -0,0 +1,5 @@
+package com.example.java.datastructure.tree;
+
+public interface Tree<E> {
+
+}
