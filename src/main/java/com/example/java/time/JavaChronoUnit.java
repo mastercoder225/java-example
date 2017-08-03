a4a79060393c10b873f047596d33c08c90c6f4d7
zhangjian <zhangjian@kongzhong.com>
2017-08-03 17:55:03 +0800

    fix

---
+++ src/main/java/com/example/java/time/JavaChronoUnit.java
@@ -0,0 +1,25 @@
+package com.example.java.time;
+
+import java.time.temporal.ChronoUnit;
+
+public class JavaChronoUnit {
+
+    public static void main(String[] args) {
+        ChronoUnit a01 = ChronoUnit.NANOS;
+        ChronoUnit a02 = ChronoUnit.MICROS;
+        ChronoUnit a03 = ChronoUnit.MILLIS;
+        ChronoUnit a04 = ChronoUnit.SECONDS;
+        ChronoUnit a05 = ChronoUnit.MINUTES;
+        ChronoUnit a06 = ChronoUnit.HOURS;
+        ChronoUnit a07 = ChronoUnit.HALF_DAYS;
+        ChronoUnit a08 = ChronoUnit.DAYS;
+        ChronoUnit a09 = ChronoUnit.WEEKS;
+        ChronoUnit a10 = ChronoUnit.MONTHS;
+        ChronoUnit a11 = ChronoUnit.YEARS;
+        ChronoUnit a12 = ChronoUnit.DECADES;
+        ChronoUnit a13 = ChronoUnit.CENTURIES;
+        ChronoUnit a14 = ChronoUnit.MILLENNIA;
+        ChronoUnit a15 = ChronoUnit.ERAS;
+        ChronoUnit a16 = ChronoUnit.FOREVER;
+    }
+}
--- src/main/java/com/example/java/time/JavaInstant.java
+++ src/main/java/com/example/java/time/JavaInstant.java
@@ -1,6 +1,8 @@
 package com.example.java.time;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public class JavaInstant {
 
@@ -14,13 +16,19 @@ public class JavaInstant {
         Instant g = Instant.MAX; // +1000000000-12-31T23:59:59.999999999Z
         Instant h = Instant.MIN; // -1000000000-01-01T00:00:00Z
 
-        Instant instant = Instant.parse("1991-02-25T00:00:00Z");
+        Instant instant = Instant.EPOCH;
 
+        Instant p1 = instant.plusNanos(1); // 1970-01-01T00:00:00.000000001Z
+        Instant p2 = instant.plusMillis(1); // 1970-01-01T00:00:00.001Z
+        Instant p3 = instant.plusSeconds(1); // 1970-01-01T00:00:01Z
+        Instant p4 = instant.plus(1, ChronoUnit.NANOS); // 1970-01-01T00:00:00.000000001Z
+        Instant p5 = instant.plus(Duration.ofNanos(1)); // 1970-01-01T00:00:00.000000001Z
 
-
-
-
-
+        Instant m1 = instant.minusNanos(1); // 1969-12-31T23:59:59.999999999Z
+        Instant m2 = instant.minusMillis(1); // 1969-12-31T23:59:59.999Z
+        Instant m3 = instant.minusSeconds(1); // 1969-12-31T23:59:59Z
+        Instant m4 = instant.minus(1, ChronoUnit.NANOS); // 1969-12-31T23:59:59.999999999Z
+        Instant m5 = instant.minus(Duration.ofNanos(1)); // 1969-12-31T23:59:59.999999999Z
 
     }
 }
